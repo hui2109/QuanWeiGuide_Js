b11a5ceff7d1d4b2e7d9545998eb0390b9138e7a
酷酷的辉 <995631664@qq.com>
2022-08-26 20:51:27 +0800

    第二章：显式转换

---
+++ 02_my_code/02_词法结构/10_显式转换.js
@@ -0,0 +1,17 @@
+let a=17;
+let binary ='0b'+a.toString(2)
+console.log(binary)
+
+console.log("-----------------------")
+
+let n=123456.789
+console.log(n.toFixed(0))
+console.log(n.toFixed(2))
+console.log(n.toFixed(5))
+console.log(n.toExponential(1))
+console.log(n.toExponential(3))
+console.log(n.toExponential(9))
+console.log(n.toPrecision(4))
+console.log(n.toPrecision(7))
+console.log(n.toPrecision(9))
+console.log(n.toPrecision(10))
