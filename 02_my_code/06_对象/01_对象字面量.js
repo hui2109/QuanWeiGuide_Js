dd6291937b4f8c550a2de534d3dd0ec8c36f6f2d
酷酷的辉 <995631664@qq.com>
2022-09-04 15:25:07 +0800

    对象学习

---
+++ 02_my_code/06_对象/01_对象字面量.js
@@ -0,0 +1,12 @@
+let empty={};
+let point={x:1,y:1};
+let p2={x:point.x,y:point.y};
+let book={
+    'main-title':'JavaScript',
+    'sub title':'The Definitive Guide',
+    for:'all audiences',
+    author:{
+        firstname:'David',
+        surname:'Flanagan'
+    }
+};
